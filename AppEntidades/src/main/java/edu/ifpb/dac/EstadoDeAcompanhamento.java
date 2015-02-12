0ade3e326ec54458eb506a15e2d85157f61ef080
Sergio Diniz <sergiodinizsh@gmail.com>
2015-02-12 10:51:48 -0300

    entidades copiadas do projeto 1

---
+++ AppEntidades/src/main/java/edu/ifpb/dac/EstadoDeAcompanhamento.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ifpb.dac;
+
+/**
+ *
+ * @author Sergiod
+ */
+public enum EstadoDeAcompanhamento {
+    AGUARDANDO, ATENDIDA, EM_TRABALHO, VERIFICADO
+    
+}
--- AppEntidades/src/main/java/edu/ifpb/dac/Pessoa.java
+++ AppEntidades/src/main/java/edu/ifpb/dac/Pessoa.java
@@ -1,45 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.ifpb.dac;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  *
- * @author Ricardo Job
+ * @author Sergiod
  */
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+
 public class Pessoa implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String nome;
-
+    @Id @GeneratedValue
+    private int id;
+    @Column (nullable = false, unique = true)
+    private String email;
+    @Column (nullable = false)
+    private String senha;
+ 
     public Pessoa() {
     }
 
-    public Pessoa(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
+    public Pessoa(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
     }
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getEmail() {
+        return email;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
